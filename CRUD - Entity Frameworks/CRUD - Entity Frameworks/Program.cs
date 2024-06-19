2fe35fc2684a0aeac7687e53b1ca977c15a17298
Gurvinder Singh <singhg389@mymacewan.ca>
2024-06-18 23:24:17 -0600

    Update Program.cs

--- CRUD - Entity Frameworks/CRUD - Entity Frameworks/Program.cs
+++ CRUD - Entity Frameworks/CRUD - Entity Frameworks/Program.cs
@@ -16,23 +16,19 @@ public class Program
     {
         UserRepository repository = new UserRepository();
 
-        // List all users initially
         Console.WriteLine("Initial user list:");
         PrintUsers(repository.ListAllUsers());
 
-        // Add a new user
         User newUser = new User { Name = "John Doe", Email = "john.doe@example.com" };
         repository.AddUser(newUser);
         Console.WriteLine("After adding a new user:");
         PrintUsers(repository.ListAllUsers());
 
-        // Update the user
         newUser.Name = "John A. Doe";
         repository.UpdateUser(newUser);
         Console.WriteLine("After updating the user:");
         PrintUsers(repository.ListAllUsers());
 
-        // Delete the user
         repository.DeleteUser(newUser.Id);
         Console.WriteLine("After deleting the user:");
         PrintUsers(repository.ListAllUsers());
